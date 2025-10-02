ce6edba5f5b75ba6bd9c2ff21393d937cba37636
AHuerva <cl.albahc@gmail.com>
2025-10-02 10:03:10 +0200

    Create launchAna.sh

---
+++ Efficiency_study/launchAna.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script sets up an environment variable and then launches restManager for a range of files.
+
+
+export NAME=aN #aN Primakoff ABC
+export PRESSURE=2.0
+export DRIFT=6
+filename="simFiles_${NAME}/RestG4_run00029_2.0bar_aN-histogram_XeNeISO_6cmDrift_v2.4.3.root" 
+# Launch restManager with the constructed filename
+echo "NAME=$NAME"
+echo "PRESSURE=$PRESSURE"
+echo "DRIFT=$DRIFT"
+echo "filename=$filename"
+ls -l "$filename"  
+
+if [ -f "$filename" ]; then
+        # Launch restManager with the constructed filename. 
+        restManager --c Ar_Analysis.rml --f "$filename" 
+    else
+        echo "File $filename does not exist."
+    fi
