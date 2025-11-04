f65ea264766865dc75a91e138085c2236d43788d
AHuerva <cl.albahc@gmail.com>
2025-11-04 08:40:42 +0100

    Update launchAna.sh

--- Efficiency_study/launchAna.sh
+++ Efficiency_study/launchAna.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+port#!/bin/bash
 
 # This script sets up an environment variable and then launches restManager for a range of files.
 
@@ -6,6 +6,7 @@
 export NAME=aN #aN Primakoff ABC
 export PRESSURE=2.0
 export DRIFT=6
+export MIXTURE=Xenon-Neon 48.85Pct-Iso 2.3Pct 10-10E3Vcm #Argon-Isobutane 1Pct 10-10E3Vcm \ Xenon-Neon 48.85Pct-Iso 2.3Pct 10-10E3Vcm
 filename="simFiles_${NAME}/RestG4_run00029_2.0bar_aN-histogram_XeNeISO_6cmDrift_v2.4.3.root" 
 # Launch restManager with the constructed filename
 echo "NAME=$NAME"
